9ecf93085f7a63c3829f24a40ed0502f933cdf75
leon.stoelt@nordisch.com <leon.stoelt@nordisch.com>
2024-08-13 10:54:57 +0200

    first comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
-
+import { HlmButtonDirective } from '@spartan-ng/button-helm';
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [HlmButtonDirective],
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  template: `<button hlmBtn variant="outline">Hello from {{ title }}</button>`,
 })
 export class AppComponent {
   title = 'testNx';
